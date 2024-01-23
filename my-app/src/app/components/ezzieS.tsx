edec548c7f584166ef44f46ce8b0506f6246c0cc
Co-Ez <edwards.ezzie@gmail.com>
2024-01-23 17:05:59 +0000

    ars

--- my-app/src/app/components/ezzieS.tsx
+++ my-app/src/app/components/ezzieS.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 export default function EzzieS() {
     return (
-        <div className="block">
-            <h1 className="justify-self-center">Let's make some bread!</h1>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU' />
-
+        <div>
+            <h1 className="text-center">Let's make some bread!</h1>
+            <div className="ml-auto mr-auto flex">
+                <img className="inline-block ml-auto mr-auto" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU' />
+            </div>
         </div>
     );
 }

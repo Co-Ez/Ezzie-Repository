9192e64dc6a01acb92a7339190f76a35788ebc92
Co-Ez <edwards.ezzie@gmail.com>
2024-01-30 12:33:47 +0000

    made box thing

--- my-app/src/app/components/ezzieS.tsx
+++ my-app/src/app/components/ezzieS.tsx
@@ -2,14 +2,29 @@ import React from "react";
 
 export default function EzzieS() {
     return (
-        <div className='overflow-auto min-h-full min-w-full' style={{
+        <div className='min-h-full min-w-full' style={{
             backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU")`,
             backgroundRepeat: `no-repeat`,
             backgroundSize: `cover`
         }}>
             <h1 className="text-center">Let's make some bread!</h1>
-            <div className="ml-auto mr-auto flex">
-                <img className="inline-block ml-auto mr-auto" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU' />
+            <div className="ml-auto mr-auto flex" style={{
+                height: `700px`,
+                width: `1500px`
+            }}>
+                <div className="overflow-auto inline-block ml-auto mr-auto">
+                    <p>0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000</p>
+                    <img className="ml-auto mr-auto" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU' />
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU' />
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU' />
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU' />
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU' />
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU' />
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU' />
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU' />
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU' />
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTVbCxkT0Q3UV36nni8epTYBb2LfvA5IqczA&usqp=CAU' />
+                </div>
             </div>
         </div>
     );

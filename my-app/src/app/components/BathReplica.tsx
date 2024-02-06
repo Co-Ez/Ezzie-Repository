dd363306e3e94fb66fc922ee5bca80e6e9feae5a
Co-Ez <edwards.ezzie@gmail.com>
2024-02-06 15:17:36 +0000

    Cool logo thing

--- my-app/src/app/components/BathReplica.tsx
+++ my-app/src/app/components/BathReplica.tsx
@@ -10,6 +10,7 @@ export default function BathReplica() {
             backgroundAttachment: `fixed`,
             backgroundSize: `cover`
         }}>
+            
             <div className="min-w-full min-h-40 max-h-40 bg-white">
                 <div className="min-w-full min-h-2 bg-blue-500"></div>
                 Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder 
@@ -54,6 +55,16 @@ export default function BathReplica() {
                 Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder 
                 Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder 
                 Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder Placeholder 
+            </div>
+            <div className="flex">
+                <img src="/img/BathBG.jpg" className="mb-40 ml-10" style={{
+                    width: `150px`,
+                    height: `150px`,
+                    border: `7px solid #FFF`,
+                    borderRadius: `24px`
+                }}></img>
+                <h1 className="text-white text-5xl ml-10 mt-3 overline decoration-amber-300">Placeholder</h1>
+                <h1 className="text-white absolute mt-16 text-lg" style={{marginLeft: `232px`}}>Placeholder</h1>
             </div>
         </div>
     );

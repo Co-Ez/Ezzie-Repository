023d4867f7ce078a49c483b4aaab49247d275cd4
Co-Ez <edwards.ezzie@gmail.com>
2024-02-13 11:08:39 +0000

    got the buttons working, very scuffed though

--- my-app/src/app/components/BathReplica.tsx
+++ my-app/src/app/components/BathReplica.tsx
@@ -3,6 +3,62 @@
 import React, { useState } from "react";
 
 export default function BathReplica() {
+    const Switch = () => {
+        document.getElementById("b1").style.color = "#00B0BD";
+        document.getElementById("b1").style.backgroundColor = "white";
+
+        document.getElementById("b2").style.color = "black";
+        document.getElementById("b2").style.backgroundColor = "";
+        
+        document.getElementById("b3").style.color = "black";
+        document.getElementById("b3").style.backgroundColor = "";
+
+        document.getElementById("b4").style.color = "black";
+        document.getElementById("b4").style.backgroundColor = "";
+    }
+
+    const Switch2 = () => {
+        document.getElementById("b2").style.color = "#00B0BD";
+        document.getElementById("b2").style.backgroundColor = "white";
+
+        document.getElementById("b1").style.color = "black";
+        document.getElementById("b1").style.backgroundColor = "";
+        
+        document.getElementById("b3").style.color = "black";
+        document.getElementById("b3").style.backgroundColor = "";
+
+        document.getElementById("b4").style.color = "black";
+        document.getElementById("b4").style.backgroundColor = "";
+    }
+
+    const Switch3 = () => {
+        document.getElementById("b3").style.color = "#00B0BD";
+        document.getElementById("b3").style.backgroundColor = "white";
+
+        document.getElementById("b1").style.color = "black";
+        document.getElementById("b1").style.backgroundColor = "";
+        
+        document.getElementById("b2").style.color = "black";
+        document.getElementById("b2").style.backgroundColor = "";
+
+        document.getElementById("b4").style.color = "black";
+        document.getElementById("b4").style.backgroundColor = "";
+    }
+
+    const Switch4 = () => {
+        document.getElementById("b4").style.color = "#00B0BD";
+        document.getElementById("b4").style.backgroundColor = "white";
+
+        document.getElementById("b1").style.color = "black";
+        document.getElementById("b1").style.backgroundColor = "";
+        
+        document.getElementById("b2").style.color = "black";
+        document.getElementById("b2").style.backgroundColor = "";
+
+        document.getElementById("b3").style.color = "black";
+        document.getElementById("b3").style.backgroundColor = "";
+    }
+
     return(
         <div className="min-h-full min-w-full flex flex-col-reverse" style={{
             backgroundImage: `url(/img/BathBG.jpg)`,
@@ -64,10 +120,10 @@ export default function BathReplica() {
                         width: `75%`,
                         backgroundColor: `#00B0BD`
                     }}>
-                    <button type="button" className="mt-auto mb-auto pl-2 pr-2 bg-white" style={{height: `56px`, color: `#00B0BD`}}>Placeholder</button>
-                    <button type="button" className="mt-auto mb-auto pl-2 pr-2" style={{height: `56px`}}>Placeholder</button>
-                    <button type="button" className="mt-auto mb-auto pl-2 pr-2" style={{height: `56px`}}>Placeholder</button>
-                    <button type="button" className="mt-auto mb-auto pl-2 pr-2" style={{height: `56px`}}>Placeholder</button>
+                    <button type="button" className="mt-auto mb-auto pl-2 pr-2" id="b1" style={{height: `56px`}} onClick={Switch}>Placeholder</button>
+                    <button type="button" className="mt-auto mb-auto pl-2 pr-2" id="b2" style={{height: `56px`}} onClick={Switch2}>Placeholder</button>
+                    <button type="button" className="mt-auto mb-auto pl-2 pr-2" id="b3" style={{height: `56px`}} onClick={Switch3}>Placeholder</button>
+                    <button type="button" className="mt-auto mb-auto pl-2 pr-2" id="b4" style={{height: `56px`}} onClick={Switch4}>Placeholder</button>
                 </div>
             </div>
             <div className="flex">
